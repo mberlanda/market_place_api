422c79f5542d7a0c70308e0f6fae1ef40a61c3b1
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-03-27 17:31:05 +0200

    Adds destroy session action added

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -38,4 +38,16 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
     end
   end
 
+  describe "DELETE #destroy" do
+
+    before(:each) do
+      @user = FactoryGirl.create :user
+      sign_in @user
+      delete :destroy, params: { id: @user.auth_token }
+    end
+
+    it { should respond_with 204 }
+
+  end
+
 end
