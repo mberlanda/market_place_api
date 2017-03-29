7894091e9c60120df182ed9d7680ec5384b75b6a
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-03-29 10:05:00 +0200

    Adds the create method for the orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :user
+
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :user_id, presence: true
 
   has_many :placements
   has_many :products, through: :placements
+
+  before_validation :set_total!
+
+
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
