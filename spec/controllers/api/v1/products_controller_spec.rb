4484bc1d488110e91ea8d955c3f8e34dedc30e57
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-04-03 13:02:40 +0200

    Refactors the pagination behavior for index actions

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -44,12 +44,7 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
       end
 
       it { should respond_with 200 }
-
-      it { expect(json_response).to have_key(:meta) }
-      it { expect(json_response[:meta]).to have_key(:pagination) }
-      it { expect(json_response[:meta][:pagination]).to have_key(:per_page) }
-      it { expect(json_response[:meta][:pagination]).to have_key(:total_pages) }
-      it { expect(json_response[:meta][:pagination]).to have_key(:total_objects) }
+      it_behaves_like 'paginated list'
     end
 
     context "when product_ids parameter is sent" do
